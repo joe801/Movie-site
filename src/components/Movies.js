f509fdc4b88148f5d5fe3f7291f3ceefc60f2b14
Joseph Uwagbale <joecrypt@yahoo.com>
2023-02-15 00:17:45 +0100

    added condition for movie list length

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,12 +6,23 @@ import Movie from "./Movie";
 
 const Movies = ( {movielist}) => {
     return ( 
-        <section className="movies">
-            {movielist.map((film) => (
-                <Link to={`/${film.imdbID}`} style={{textDecoration: 'none', color: '#EBE5E2'}}>
-                    <Movie Title={film.Title} Poster={film.Poster} Year={film.Year} key={film.imdbID}/>
-                </Link>
-            ))}
+        <section>
+            {
+                movielist?.length > 0 
+                ? (
+                    <div className="movies">
+                    {movielist.map((film) => (
+                        <Link to={`/${film.imdbID}`} style={{textDecoration: 'none', color: '#EBE5E2'}}>
+                            <Movie Title={film.Title} Poster={film.Poster} Year={film.Year} key={film.imdbID}/>
+                        </Link>
+                    ))}
+                    </div> 
+                ): (
+                <div className="noMovie">
+                    <h2>No movie Found</h2>
+                </div>
+                )
+            }
         </section>
      );
 }
